492ebb87440a632492b55bfcbec688bd8d351bc7
NathanaelKhor <nwxkhor@gmail.com>
2025-02-07 11:51:37 +1100

    Finished

--- main.py
+++ main.py
@@ -10,8 +10,18 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def solve(board):
+def solve(board: list[list[int]]) -> bool:
+    """
+    Recursively solves the board square by square.  Starts by checking the first empty square and trying every number until a valid one is found, then moves onto the next square and repeating the process
+    If there are no valid numbers in the second square then it moves back to the first recursive call and continues to move up the numbers until next valid one is found and then moves on.  
+    The base case is when there are no empty squares left as that is when the board is fully solved.
 
+    Args:
+        board (list[list[int]]): A nested list where each internal list represents a row and the numbers in those lists represent the number in that row, column slot
+
+    Returns:
+        Boolean: Returns true if there is a valid number to place in the current slot, allowing the recursion to move onto the next call.  Returns false if there are not valid slots, moving back to the previous case.
+    """
     find = find_empty(board)
     if not find:
         return True
@@ -30,7 +40,18 @@ def solve(board):
     return False
     
 
-def valid(board, num, pos) -> bool:
+def valid(board: list[list[int]], num: int, pos: tuple[int, int]) -> bool:
+    """
+    Checks to see if the chosen number can be placed in that square.  Checks the row, column and 3x3 square that the number is being placed in to see that it is unique.
+
+    Args:
+        board (list[list[int]]): A nested list where each internal list represents a row and the numbers in those lists represent the number in that row, column slot
+        num (int): The number to be placed on the board
+        pos (tuple[int, int]): A tuple containing the 'coordinates' of the selected square (row, col)
+
+    Returns:
+        bool: Returns true if the position is valid for the number, false if not
+    """
 
     # Check row
     for i in range(len(board[0])):
@@ -54,7 +75,13 @@ def valid(board, num, pos) -> bool:
     return True
         
 
-def print_board(board):
+def print_board(board: list[list[int]]):
+    """
+    Prints the sudoku board
+
+    Args:
+        board (list[list[int]]): A nested list where each internal list represents a row and the numbers in those lists represent the number in that row, column slot
+    """
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - ")
@@ -68,16 +95,29 @@ def print_board(board):
             else:
                 print(str(board[i][j]) + " ", end="")
 
-def find_empty(board) -> tuple[int, int]:
+def find_empty(board: list[list[int]]) -> tuple[int, int] | None:
+    """
+    Finds the first empty spot on the board by searching through the rows and the columns
+
+    Args:
+        board (list[list[int]]): A nested list where each internal list represents a row and the numbers in those lists represent the number in that row, column slot
+
+    Returns:
+        tuple[int, int] | None: Returns the coordinates of the empty spot if there is one (row, col), if there are no empty spots returns None
+    """
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
                 return (i, j) # row, col
     
     return None
-    
-print_board(board)
-solve(board)
-print('_______________________')
-print('')
-print_board(board)
+
+def main():
+    print_board(board)
+    solve(board)
+    print('_______________________')
+    print('')
+    print_board(board)
+
+if __name__ == "__main__":
+    main()
